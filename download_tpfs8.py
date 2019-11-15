c470b94ef01f805624c8c03d5aed01ddccb79ce7
Jose Perez <rayzote@email.arizona.edu>
2019-11-15 05:40:18 -0700

    :art: Writing original summer exploration aperture to FITS :recycle: Reformatting

--- download_tpfs8.py
+++ download_tpfs8.py
@@ -7,25 +7,36 @@ TPF Naming notation:
 """
 from lightkurve import search_tesscut
 from astropy.io import ascii
+from usefulFuncs import getOrigAps
+import numpy as np
 
-# Parameters
 sector = 8
 cutout_size = 8
 
 # Import target list
 targets = ascii.read('DataInput/cluster_targets_tic.ecsv')
+gmin = targets['G Group'].min()
+
+# Get original apertures
+group_apts = getOrigAps(targets)
 
 # Filename template
 template_fn = './TESSCuts/TPF{ticid}_S{sec}C{cutsize}.fits'
 
 # Download each TESS cuts for each target
-for target in targets:
-    ticid = target['TIC ID']
-    
+for ticid, Ggroup in targets[['TIC ID', 'G Group']]:
+    # Get aperture and convert to Keppler/Tess format
+    apt = group_apts[Ggroup - gmin] + 1
+    apt[apt == 2] += 1
+
     print(f'Downloading TIC ID={ticid}')
     tpf = search_tesscut(ticid, sector=sector).download(cutout_size=cutout_size)
+
+    # Write TIC ID and pipeline aperture to HDUs
     tpf.hdu[0].header['TICID'] = ticid
+    tpf.hdu[2].data = apt
+
     tpf_fn = template_fn.format(ticid=ticid, sec=sector, cutsize=cutout_size)
-    
+
     print(f'Saving to {tpf_fn}')
     tpf.to_fits(output_fn=tpf_fn, overwrite=True)
